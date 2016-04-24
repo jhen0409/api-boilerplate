902208b509c6d88d742453c78545681371be5858
Jhen <developer@jhen.me>
2016-04-24 09:49:32 +0800

    Use webpack for production build

--- src/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  require('source-map-support').install();
-}
-
-require('./server');
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/server',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'server.js',
+    libraryTarget: 'commonjs2'
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/\.(md)$/),
+    new webpack.IgnorePlugin(/LICENSE$/),
+    new webpack.BannerPlugin(
+      'require("source-map-support").install();',
+      { raw: true, entryOnly: false }
+    ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.json$/,
+      loader: 'json'
+    }]
+  },
+  target: 'node',
+  node: {
+    __dirname: true,
+    __filename: true
+  },
+  externals: [
+    'bcrypt',
+    'mongoose',
+    'redis'
+  ]
+};
